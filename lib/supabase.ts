a99b0870d90c719fd2a802e74a9d85ba722cac8d
Mish Ushakov <mishushakov@users.noreply.github.com>
2024-08-09 15:45:31 +0200

    interface changes

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = !process.env.NEXT_PUBLIC_DISABLE_SUPABASE
+export const supabase = process.env.NEXT_PUBLIC_ENABLE_SUPABASE
   ? createClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
