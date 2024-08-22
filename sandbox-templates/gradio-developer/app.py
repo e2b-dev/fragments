06557a9db47a56d5ebe209a86c5d3e7eedc2d7c3
Mish Ushakov <mishushakov@users.noreply.github.com>
2024-08-22 16:03:02 +0200

    added gradio support

---
+++ sandbox-templates/gradio-developer/app.py
@@ -0,0 +1,13 @@
+# Scaffolding for a Streamlit app
+import gradio as gr
+
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+)
+
+demo.launch()
