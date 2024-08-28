596a6589a0fba67a3216c057a9984b9291c0215b
Mish Ushakov <mishushakov@users.noreply.github.com>
2024-08-28 17:18:04 +0200

    disable posthog on localhost

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { CSPostHogProvider } from './providers'
+import { PostHogProvider } from './providers'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,11 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <CSPostHogProvider>
+      <PostHogProvider>
         <body className={inter.className}>
           {children}
         </body>
-      </CSPostHogProvider>
+      </PostHogProvider>
     </html>
   )
 }
