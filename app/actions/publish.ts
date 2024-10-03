6ead476a5740bf6dd34b374a560b2257090659d9
Mish Ushakov <mishushakov@users.noreply.github.com>
2024-10-03 21:24:55 +0200

    added custom expiration picker

--- app/actions/publish.ts
+++ app/actions/publish.ts
@@ -1,21 +1,23 @@
 'use server'
 
+import { Duration, ms } from '@/lib/duration'
 import { Sandbox } from '@e2b/code-interpreter'
 import { kv } from '@vercel/kv'
 import { customAlphabet } from 'nanoid'
 
 const nanoid = customAlphabet('1234567890abcdef', 7)
-const sandboxTimeout = 3 * 60 * 60 * 1000 // 3 hours
 
 export async function publish(
   url: string,
   sbxId: string,
+  duration: Duration,
   apiKey: string | undefined,
 ) {
   if (process.env.KV_REST_API_URL && process.env.KV_REST_API_TOKEN) {
     const id = nanoid()
-    await kv.set(`fragment:${id}`, url, { px: sandboxTimeout })
-    await Sandbox.setTimeout(sbxId, sandboxTimeout, { apiKey })
+    const expiration = ms(duration)
+    await kv.set(`fragment:${id}`, url, { px: expiration })
+    await Sandbox.setTimeout(sbxId, expiration, { apiKey })
 
     return {
       url: process.env.NEXT_PUBLIC_SITE_URL
