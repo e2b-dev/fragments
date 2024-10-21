b78d15eb23e9224cebebe328bf38e14791afde69
Mish Ushakov <mishushakov@users.noreply.github.com>
2024-10-21 15:36:06 +0200

    sdk v1 + code interpreter v1

--- app/api/sandbox/route.ts
+++ app/api/sandbox/route.ts
@@ -1,6 +1,6 @@
 import { FragmentSchema } from '@/lib/schema'
 import { ExecutionResultInterpreter, ExecutionResultWeb } from '@/lib/types'
-import { Sandbox, CodeInterpreter } from '@e2b/code-interpreter'
+import { Sandbox } from '@e2b/code-interpreter'
 
 const sandboxTimeout = 10 * 60 * 1000 // 10 minute in ms
 
@@ -15,76 +15,55 @@ export async function POST(req: Request) {
     await req.json()
   console.log('fragment', fragment)
   console.log('userID', userID)
-  console.log('apiKey', apiKey)
-
-  let sbx: Sandbox | CodeInterpreter | undefined = undefined
+  // console.log('apiKey', apiKey)
 
   // Create a interpreter or a sandbox
-  if (fragment.template === 'code-interpreter-multilang') {
-    sbx = await CodeInterpreter.create({
-      metadata: { template: fragment.template, userID: userID },
-      timeoutMs: sandboxTimeout,
-      apiKey,
-    })
-    console.log('Created code interpreter', sbx.sandboxID)
-  } else {
-    sbx = await Sandbox.create(fragment.template, {
-      metadata: { template: fragment.template, userID: userID },
-      timeoutMs: sandboxTimeout,
-      apiKey,
-    })
-    console.log('Created sandbox', sbx.sandboxID)
-  }
+  const sbx = await Sandbox.create(fragment.template, {
+    metadata: { template: fragment.template, userID: userID },
+    timeoutMs: sandboxTimeout,
+    apiKey,
+  })
 
   // Install packages
   if (fragment.has_additional_dependencies) {
-    if (sbx instanceof CodeInterpreter) {
-      await sbx.notebook.execCell(fragment.install_dependencies_command)
-      console.log(
-        `Installed dependencies: ${fragment.additional_dependencies.join(', ')} in code interpreter ${sbx.sandboxID}`,
-      )
-    } else if (sbx instanceof Sandbox) {
-      await sbx.commands.run(fragment.install_dependencies_command)
-      console.log(
-        `Installed dependencies: ${fragment.additional_dependencies.join(', ')} in sandbox ${sbx.sandboxID}`,
-      )
-    }
+    await sbx.commands.run(fragment.install_dependencies_command)
+    console.log(
+      `Installed dependencies: ${fragment.additional_dependencies.join(', ')} in sandbox ${sbx.sandboxId}`,
+    )
   }
 
   // Copy code to fs
   if (fragment.code && Array.isArray(fragment.code)) {
     fragment.code.forEach(async (file) => {
       await sbx.files.write(file.file_path, file.file_content)
-      console.log(`Copied file to ${file.file_path} in ${sbx.sandboxID}`)
+      console.log(`Copied file to ${file.file_path} in ${sbx.sandboxId}`)
     })
   } else {
     await sbx.files.write(fragment.file_path, fragment.code)
-    console.log(`Copied file to ${fragment.file_path} in ${sbx.sandboxID}`)
+    console.log(`Copied file to ${fragment.file_path} in ${sbx.sandboxId}`)
   }
 
   // Execute code or return a URL to the running sandbox
-  if (fragment.template === 'code-interpreter-multilang') {
-    const result = await (sbx as CodeInterpreter).notebook.execCell(
-      fragment.code || '',
-    )
-    await (sbx as CodeInterpreter).close()
+  if (fragment.template === 'code-interpreter-v1') {
+    const { logs, error, results } = await sbx.runCode(fragment.code || '')
+
     return new Response(
       JSON.stringify({
-        sbxId: sbx?.sandboxID,
+        sbxId: sbx?.sandboxId,
         template: fragment.template,
-        stdout: result.logs.stdout,
-        stderr: result.logs.stderr,
-        runtimeError: result.error,
-        cellResults: result.results,
+        stdout: logs.stdout,
+        stderr: logs.stderr,
+        runtimeError: error,
+        cellResults: results,
       } as ExecutionResultInterpreter),
     )
-  } else {
-    return new Response(
-      JSON.stringify({
-        sbxId: sbx?.sandboxID,
-        template: fragment.template,
-        url: `https://${sbx?.getHost(fragment.port || 80)}`,
-      } as ExecutionResultWeb),
-    )
   }
+
+  return new Response(
+    JSON.stringify({
+      sbxId: sbx?.sandboxId,
+      template: fragment.template,
+      url: `https://${sbx?.getHost(fragment.port || 80)}`,
+    } as ExecutionResultWeb),
+  )
 }
--- components/fragment-preview.tsx
+++ components/fragment-preview.tsx
@@ -5,7 +5,7 @@ import { FragmentWeb } from './fragment-web'
 import { ExecutionResult } from '@/lib/types'
 
 export function FragmentPreview({ result }: { result: ExecutionResult }) {
-  if (result.template === 'code-interpreter-multilang') {
+  if (result.template === 'code-interpreter-v1') {
     return <FragmentInterpreter result={result} />
   }
 
