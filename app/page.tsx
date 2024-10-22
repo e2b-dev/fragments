aac27a4612c890750da326bda0ee1106cb6f7900
Mish Ushakov <mishushakov@users.noreply.github.com>
2024-10-22 23:42:16 +0200

    new sonnet as default

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
   const [languageModel, setLanguageModel] = useLocalStorage<LLMModelConfig>(
     'languageModel',
     {
-      model: 'claude-3-5-sonnet-20240620',
+      model: 'claude-3-5-sonnet-latest',
     },
   )
 
