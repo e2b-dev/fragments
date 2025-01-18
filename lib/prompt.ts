06bf87bcf0fac5f80b24cef345af0322fb11987c
Mish Ushakov <10400064+mishushakov@users.noreply.github.com>
2025-01-18 17:30:46 +0100

    fixes regression after mistral model was added (#98)
    Co-authored-by: Mish Ushakov <mishushakov@users.noreply.github.com>

--- lib/prompt.ts
+++ lib/prompt.ts
@@ -8,7 +8,7 @@ export function toPrompt(template: Templates) {
     You can install additional dependencies.
     Do not touch project dependencies files like package.json, package-lock.json, requirements.txt, etc.
     Do not wrap code in backticks.
-    Break new lines with \\n.
+    Always break the lines correctly.
     You can use one of the following templates:
     ${templatesToPrompt(template)}
   `
