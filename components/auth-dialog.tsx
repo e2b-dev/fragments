ba0177941ed495b9bd9e23c840e279852de65c13
Mish <10400064+mishushakov@users.noreply.github.com>
2025-04-22 16:11:25 +0200

    moved auth-form to auth-dialog

--- components/auth-dialog.tsx
+++ components/auth-dialog.tsx
@@ -1,6 +1,6 @@
-import AuthForm from './auth-form'
+import Auth, { ViewType } from './auth'
+import Logo from './logo'
 import { Dialog, DialogContent, DialogTitle } from '@/components/ui/dialog'
-import { ViewType } from '@/components/auth'
 import { VisuallyHidden } from '@radix-ui/react-visually-hidden'
 import { SupabaseClient } from '@supabase/supabase-js'
 
@@ -21,7 +21,28 @@ export function AuthDialog({
         <VisuallyHidden>
           <DialogTitle>Sign in to E2B</DialogTitle>
         </VisuallyHidden>
-        <AuthForm supabase={supabase} view={view} />
+        <div className="flex justify-center items-center flex-col">
+          <h1 className="flex items-center gap-4 text-xl font-bold mb-6 w-full">
+            <div className="flex items-center justify-center rounded-md shadow-md bg-black p-2">
+              <Logo className="text-white w-6 h-6" />
+            </div>
+            Sign in to Fragments
+          </h1>
+          <div className="w-full">
+            <Auth
+              supabaseClient={supabase}
+              view={view}
+              providers={['github', 'google']}
+              socialLayout="horizontal"
+              onSignInValidate={async (email, password) => {
+                if (email.endsWith('@e2b.dev')) {
+                  return
+                }
+                throw new Error('Invalid email domain')
+              }}
+            />
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   )
