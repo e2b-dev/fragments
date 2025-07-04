f80f38c276cafc6da4c0d3e427d8beddae94f3f5
Muhammad Hamid Raza <itmughal2023@gmail.com>
2025-07-04 17:24:49 +0500

    Refactor code structure for improved readability and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" type="image/png" href="/pwa-assets/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/pwa-assets/favicon.svg" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/pwa-assets/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="Fragments" />
+<link rel="manifest" href="/manifest.json" />
       </head>
       <PostHogProvider>
         <body className={inter.className}>
