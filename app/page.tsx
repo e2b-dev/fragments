050fe057d08e6ec5874c442c2656ed57e9010f4a
Mish Ushakov <10400064+mishushakov@users.noreply.github.com>
2025-08-17 01:41:19 +0200

    Change X link to @e2b (#167)

--- app/page.tsx
+++ app/page.tsx
@@ -230,9 +230,9 @@ export default function Home() {
     if (target === 'github') {
       window.open('https://github.com/e2b-dev/fragments', '_blank')
     } else if (target === 'x') {
-      window.open('https://x.com/e2b_dev', '_blank')
+      window.open('https://x.com/e2b', '_blank')
     } else if (target === 'discord') {
-      window.open('https://discord.gg/U7KEcGErtQ', '_blank')
+      window.open('https://discord.gg/e2b', '_blank')
     }
 
     posthog.capture(`${target}_click`)
