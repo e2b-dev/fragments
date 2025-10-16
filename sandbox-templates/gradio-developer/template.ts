18348b76834677c84fff7b0bef73c546d429fc29
Mish <10400064+mishushakov@users.noreply.github.com>
2025-10-16 17:01:44 +0200

    curl consistentcy

--- sandbox-templates/gradio-developer/template.ts
+++ sandbox-templates/gradio-developer/template.ts
@@ -2,7 +2,7 @@ import { Template, waitForPort } from 'e2b'
 
 export const template = Template()
   .fromPythonImage('3.9-slim')
-  .aptInstall(['curl']) // required for waitForPort(), we should probably include this in base image
+  .aptInstall('curl') // required for waitForPort(), we should probably include this in base image
   .pipInstall('gradio pandas numpy matplotlib requests seaborn plotly')
   .setWorkdir('/home/user')
   .copy('app.py', 'app.py')
